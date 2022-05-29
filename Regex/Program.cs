72793b960afcd13143a79f77fb6430398222714c
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-29 22:05:27 +0530

    [Biplab] Add: Validation of first name

---
+++ Regex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RegexValidation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welocme to Regex");
+            UserRegistration userRegistration = new UserRegistration();
+            //uc1 : first  name
+            Console.WriteLine("Enter the First Name : ");
+            string First_Name = Console.ReadLine();
+            userRegistration.ValidateFirstName(First_Name);
+        }
+    }
+}
