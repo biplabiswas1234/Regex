34e86df019ba82c3877a971cfc1e3b9ae63ad2ee
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-29 22:13:33 +0530

    [Biplab] Add: Validation of email id

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -8,9 +8,9 @@ namespace RegexValidation
         {
             Console.WriteLine("Welocme to Regex");
             UserRegistration userRegistration = new UserRegistration();
-            Console.WriteLine("Enter the Last Name");
-            string Last_Name = Console.ReadLine();
-            userRegistration.ValidateLastName(Last_Name);
+            Console.WriteLine("Enter the EmailId");
+            string EmailId = Console.ReadLine();
+            userRegistration.ValidateEmailId(EmailId);
 
         }
     }
